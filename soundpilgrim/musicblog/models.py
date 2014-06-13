eea86c8a6eeac3bc635de192b9fafab5e59e7bb3
John Pugliesi <jpugliesi12@gmail.com>
2014-06-13 15:18:48 -0700

    Create basic SongPost, SongCarousel, and Genre models

--- soundpilgrim/musicblog/models.py
+++ soundpilgrim/musicblog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class SongCarousel(models.Model):
+    song_carousel_title = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.song_carousel_title
+
+class Genre(models.Model):
+    genre_name = models.CharField('Genre', max_length = 50, null=False)
+
+    def __unicode__(self):
+        return self.genre_name
+
+class SongPost(models.Model):
+    created_on = models.DateField('Date Created', null=False, editable=True)
+    song_title = models.CharField(max_length=200, null=False)
+    artist = models.CharField(max_length=200, null=False)
+    genre = models.ForeignKey(Genre, null=False)
+    soundcloud_url = models.TextField(null=False)
+    song_carousel = models.ForeignKey(SongCarousel, blank=True, null=True)
+
+    def is_in_song_carousel(self):
+        return self.song_carousel
+    is_in_song_carousel.boolean = True
+    is_in_song_carousel.short_description = 'In Song Carousel?'
+
+    def __unicode__(self):
+        return self.song_title + ' ' + self.artist + ' (' + unicode(self.genre) + ')'
