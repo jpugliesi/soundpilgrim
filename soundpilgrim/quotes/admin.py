44d67215e3b2200c9c75753a1d8a1c698b977f6f
John Pugliesi <jpugliesi12@gmail.com>
2014-06-16 11:35:40 -0700

    Create admin page for Quotes

--- soundpilgrim/quotes/admin.py
+++ soundpilgrim/quotes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from quotes.models import Quote
 
-# Register your models here.
+class QuoteAdmin(admin.ModelAdmin):
+    fields = ['created_on', 'text', 'quoter']
+    list_display = ('quoter', 'text', 'created_on')
+    list_filter = ['created_on']
+
+    search_fields = ['song_title', 'artist']
+
+admin.site.register(Quote, QuoteAdmin)
--- soundpilgrim/soundpilgrim/views.py
+++ soundpilgrim/soundpilgrim/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from quotes.models import Quote
 
 def home(request):
-    
-    return render(request, 'index.html')
+    recent_quote = Quote.objects.all().order_by('created_on')[0]
+    context = {'recent_quote': recent_quote}
+    return render(request, 'index.html', context)
