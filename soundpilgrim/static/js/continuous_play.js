c789d8af926ebe1e5eccbc8c0999407fc7c4ef46
= <jpugliesi12@gmail.com>
2014-12-24 23:23:46 +0000

    Add continousPlay loading on second to last song play, and fix to homepage border

--- soundpilgrim/static/js/continuous_play.js
+++ soundpilgrim/static/js/continuous_play.js
@@ -1,16 +1,26 @@
-var tracks = [];
-var widgets = $('iframe');
-var num_widgets = widgets.length;
+function continuousPlayEval(){
+	console.log("This is a test");
+	var tracks = [];
+	var widgets = $('iframe');
+	var num_widgets = widgets.length;
 
-widgets.each(function(index, value){ 
-	tracks.push(SC.Widget(value));
-	if(index < num_widgets-1){
-		tracks[index].bind(SC.Widget.Events.READY, function(){
-			tracks[index].bind(SC.Widget.Events.FINISH, function(){
-				setTimeout(function(){
-					tracks[index+1].play();
-				}, 1000);
+	widgets.each(function(index, value){ 
+		tracks.push(SC.Widget(value));
+		if(index < num_widgets-1){
+			tracks[index].bind(SC.Widget.Events.READY, function(){
+				tracks[index].bind(SC.Widget.Events.FINISH, function(){
+					setTimeout(function(){
+						tracks[index+1].play();
+					}, 1000);
+				});
 			});
-		});
-	}
- });
+			if(index == num_widgets-2){
+			    //For the second to last widget, once play starts, load next page
+			    tracks[index].bind(SC.Widget.Events.PLAY, function(){
+				console.log("Load Next Page");
+				$('.endless_more').click();
+			    });
+			}
+		}
+	 });
+}
